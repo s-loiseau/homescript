f96a0092b5536fba95ac6c8381c764e791c89249
Stephane <Stephane@Workstation>
2016-07-18 10:07:29 +0200

    starttmux detect sessions

--- starttmux.sh
+++ starttmux.sh
@@ -1,8 +1,66 @@
 #!/bin/bash
-sn="BASE"
-tmux -2 new-session -s $sn -n CODE -d
-tmux split-window -h -t "$sn:1"
-tmux split-window -v -t "$sn:1"
-tmux new-window -t "$sn" -n NEXT
-tmux attach -t "$sn"
+# NAME    : Tmux session builder
+# COMMENT : tmux script to create a session with a few windows and panes
+# AUTHOR  : S.LOISEAU
+# CONTACT : 
+# DATE    : 23 JUIN 2016
+#
+# USAGE   : ./tmuxbuild.sh [sessionname]
 
+
+#session=$(echo $REMOTEHOST|cut -d. -f1)
+session=$(echo $HOSTNAME|cut -d. -f1)
+[ $1 ] && session=$1
+
+testtmux=$(tmux ls | cut -d: -f1|grep -cw $session)
+
+focusfirstpane () {
+    tmux select-pane -t $session:.0
+}
+
+splitH () {
+    tmux split-window -h -t $session:.
+}
+
+splitV () {
+    tmux split-window -v -t $session:.
+}
+
+if [ $testtmux -gt 0 ];then
+    # SESSION ALREADY EXIST attach and quit
+    echo SESSION ALREADY THERE
+    tmux attach -t $session
+    exit
+else
+    # CREATE SESSION
+    echo CREATE SESSION $session
+    tmux -2 new-session -s $session -n HOME -d
+fi
+
+# window 1 HOME
+[ -f GitCheck.sh ] && tmux send-keys -t $session:. 'GitCheck.sh' enter
+##pane 1.1
+splitH
+##pane 1.2
+splitV
+focusfirstpane
+
+# window 2 code
+tmux new-window -n CODE
+##pane 2.1
+splitH
+##pane 2.2
+splitV
+focusfirstpane
+
+# window 3 projet
+tmux new-window -n PROJETS
+##pane 3.1
+splitH
+focusfirstpane
+
+# Focus on first window
+tmux select-window -t $session:1
+focusfirstpane
+
+tmux attach -t $session
