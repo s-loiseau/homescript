ce842db49dddf39b3b9c83af8da9d3e6d9c17d6e
homeL <tjahl013@gmail.com>
2016-08-21 17:03:02 +0200

    gicheck rename var status2

--- gitcheck.sh
+++ gitcheck.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#source git-prompt.sh to use __git_ps1
 . ~/scripts/.git-prompt.sh
 
 currdir=$(pwd)
@@ -15,14 +16,14 @@ gitcheck() {
     cd $reporoot/$1
     git fetch
     changes=$(git status -bs)
-    status2=$(__git_ps1)
+    status=$(__git_ps1)
 
     if [ $(echo "$changes" | wc -l) -eq 1 ];then
         color=${green}
     else
         color=${red}
     fi
-    status="$status2"
+
     printf "\n>>> ${color}%-15s %10s${nc}\n$changes\n" $x "$status"
 }
 
